f0173d4b7f9a989acf602b7e2ccb27ef078ac16f
DrummerHead <drummerhead@gmail.com>
2013-07-21 18:46:40 -0300

    Commit first version

---
+++ calculate.rb
@@ -0,0 +1,51 @@
+require "./timesheet"
+
+# Define rate and currency kind
+#
+worked_time = []
+@rate = 60
+currency = 'USD'
+
+
+# Convert time string to minutes integer
+#
+def hours_to_mins(time)
+  time_inspect = time.split(':')
+  mins = time_inspect[0].to_i * 60 + time_inspect[1].to_i
+end
+
+
+# Calculate total min worked, optionally return as hours
+#
+def worked_mins(step, hours = false)
+  start_m = hours_to_mins(step[:start])
+  finish_m = hours_to_mins(step[:finish])
+  substract_m = hours_to_mins(step[:substract])
+  worked_m = finish_m - start_m - substract_m
+  return hours ? worked_m / 60.0 : worked_m
+end
+
+
+# Churn data, puts Markdown
+#
+@timesheet.each do |date, data|
+  puts "\n\n#{date.to_s.gsub('d', '')}\n=========="
+
+  data.each do |step|
+    puts step[:description].gsub(/ {8}/, '')
+    worked_time << worked_mins(step)
+    puts "__#{worked_mins(step, true)} hours__"
+  end
+end
+
+
+# Grand total calculation
+#
+total_worked_time = worked_time.reduce(:+)
+total_worked_time_in_hours = total_worked_time / 60.0
+pay_total = total_worked_time * @rate / 60.0
+
+puts "\n\nTotal\n=====\n"
+puts "#{total_worked_time}min worked = #{total_worked_time_in_hours} hours"
+puts "At #{@rate} #{currency} per hour is:"
+puts "__#{pay_total} #{currency}__"
---
+++ timesheet.rb
@@ -0,0 +1,59 @@
+@timesheet = {
+  d2013_07_17: [
+    {
+      start: '14:00',
+      finish: '18:00',
+      substract: '0',
+      description: '
+        Update note about declarative etags in release notes;
+      '
+    }
+  ],
+  d2013_07_18: [
+    {
+      start: '10:30',
+      finish: '19:00',
+      substract: '1:30',
+      description: '
+        Fix test name typos;
+        Use the superclass implementation;
+        Remove duplication in test;
+        Refactor to reduce number of loops;
+      '
+    }
+  ],
+  d2013_07_19: [
+    {
+      start: '12:00',
+      finish: '14:45',
+      substract: '0',
+      description: '
+        Meeting with Bill Clinton;
+        Use Relation#merge to combine scope chain items;
+        Remove ambiguity with pluralizations and I18n keys;
+      '
+    },
+    {
+      start: '16:00',
+      finish: '18:30',
+      substract: '0',
+      description: '
+        Fix visibility of the relation construction methods;
+        Return local time for backwards compatibility;
+        Fix link_to with block and url_hash;
+      '
+    }
+  ]
+}
+
+=begin
+  d2013_: [
+    {
+      start: '',
+      finish: '',
+      substract: '',
+      description: '
+      '
+    }
+  ],
+=end
